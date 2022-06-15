c73eaaa666bd5812e8b6f9d40af5f238f757e89d
BhautikPethani <c0854487@mylambton.ca>
2022-06-15 19:43:45 -0400

    Model class has been created and first page has been designed

---
+++ app/src/main/java/com/example/fa_bhautikpethani_c0854487_android/Model/Place.java
@@ -0,0 +1,31 @@
+package com.example.fa_bhautikpethani_c0854487_android.Model;
+
+public class Place {
+    private int id;
+    private String placeAddress;
+    private double latitude;
+    private double longitude;
+    private boolean status;
+
+    public Place(int id, String placeAddress, double latitude, double longitude, boolean status){
+        this.id = id;
+        this.placeAddress = placeAddress;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.status = status;
+    }
+
+    public int getId(){return id;}
+
+    public void setPlaceAddress(String address){this.placeAddress = address;}
+    public String getPlaceAddress(){return placeAddress;}
+
+    public void setLatitude(Double latitude){this.latitude = latitude;}
+    public double getLatitude() {return latitude;}
+
+    public void setLongitude(Double longitude){this.longitude = longitude;}
+    public double getLongitude(){return longitude;}
+
+    public void setStatus(Boolean status){this.status = status;}
+    public boolean getStatus() {return status;}
+}
